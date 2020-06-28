bcf3648f1d1cb0bed4ed82897d4643fa8a23b9f2
PiyushBatra <piyushbatra1999@gmail.com>
2020-06-28 13:41:59 +0530

    reinstall removed apps

---
+++ reinstall.cmd
@@ -0,0 +1,22 @@
+adb shell cmd package install-existing com.android.browser
+adb shell cmd package install-existing com.miui.bugreport
+adb shell cmd package install-existing com.miui.notes
+adb shell cmd package install-existing com.miui.player
+adb shell cmd package install-existing com.xiaomi.midrop
+adb shell cmd package install-existing com.xiaomi.mipicks
+adb shell cmd package install-existing com.xiaomi.scanner
+adb shell cmd package install-existing com.miui.msa.global
+adb shell cmd package install-existing com.xiaomi.glgm
+adb shell cmd package install-existing com.xiaomi.payment
+adb shell cmd package install-existing com.miui.yellowpage
+adb shell cmd package install-existing com.facebook.services
+adb shell cmd package install-existing com.facebook.system
+adb shell cmd package install-existing com.facebook.apcmd packageanager
+adb shell cmd package install-existing com.miui.cloudservice
+adb shell cmd package install-existing com.miui.cloudbackup
+adb shell cmd package install-existing com.mipay.wallet.in
+adb shell cmd package install-existing com.miui.videoplayer
+adb shell cmd package install-existing com.miui.micloudsync
+adb shell cmd package install-existing com.miui.miservice
+
+
