737747e0e28811f8c5606eabc6e2a25803acf811
PiyushBatra <piyushbatra1999@gmail.com>
2020-06-28 13:41:30 +0530

    remove bloat windows

---
+++ mi_bloat_removal.cmd
@@ -0,0 +1,22 @@
+adb shell pm uninstall -k --user 0 com.android.browser
+adb shell pm uninstall -k --user 0 com.miui.bugreport
+adb shell pm uninstall -k --user 0 com.miui.notes
+adb shell pm uninstall -k --user 0 com.miui.player
+adb shell pm uninstall -k --user 0 com.xiaomi.midrop
+adb shell pm uninstall -k --user 0 com.xiaomi.mipicks
+adb shell pm uninstall -k --user 0 com.xiaomi.scanner
+adb shell pm uninstall -k --user 0 com.miui.msa.global
+adb shell pm uninstall -k --user 0 com.xiaomi.glgm
+adb shell pm uninstall -k --user 0 com.xiaomi.payment
+adb shell pm uninstall -k --user 0 com.miui.yellowpage
+adb shell pm uninstall -k --user 0 com.facebook.services
+adb shell pm uninstall -k --user 0 com.facebook.system
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall -k --user 0 com.miui.cloudservice
+adb shell pm uninstall -k --user 0 com.miui.cloudbackup
+adb shell pm uninstall -k --user 0 com.mipay.wallet.in
+adb shell pm uninstall -k --user 0 com.miui.videoplayer
+adb shell pm uninstall -k --user 0 com.miui.micloudsync
+adb shell pm uninstall -k --user 0 com.miui.miservice
+
+
